7d575f03a6d57b21e9a220e444e7744a0d5836a6
Ricky <52875633+RICKYLIKEBIRD@users.noreply.github.com>
2023-12-25 00:21:45 +0800

    guncion跟flask的log綁定

--- app.py
+++ app.py
@@ -49,8 +49,11 @@ def handle_message(event):
     app.logger.info(event)
     line_bot_api.reply_message(event.reply_token,message)
 
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    app.logger.setLevel(logging.INFO)
+    
